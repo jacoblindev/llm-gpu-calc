bef914b00de755b21c5dc02cbcfb63df67d7638a
Jacob Lin <jacoblindev@gmail.com>
2025-09-21 20:28:34 +0800

    Revert "Tasks 3.0 - Control Dock (#21)" (#23)
    This reverts commit 472f503f90c945ff63a7add718df0418eef3d565.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import { DashboardShell } from '@ui'
-import { isV3ShellEnabled } from './flags'
-import { useAppStore } from '@app'
 import './styles/index.css'
 
-const Root = isV3ShellEnabled() ? DashboardShell : App
-const pinia = createPinia()
-const app = createApp(Root)
-app.use(pinia)
-
-// Initialize store when v3 shell is enabled (no UI code touches URL/LS directly)
-if (isV3ShellEnabled()) {
-  const store = useAppStore()
-  store.init()
-  store.loadUnitPreference()
-  store.startViewPrefsSync()
-}
-
+const app = createApp(App)
+app.use(createPinia())
 app.mount('#app-root')
+
